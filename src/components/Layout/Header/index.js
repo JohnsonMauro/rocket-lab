8c1e5464d6980185ab7ee69e8e7eb88cb7d84fa2
Johnson Mauro <johnsonmauro@gmail.com>
2020-05-24 03:05:10 -0300

    Adding logo image :zap:

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,11 +1,9 @@
+import CrunchAccounting from '@images/crunchaccounting.png';
 import { HeaderStyles, HeaderImg } from './styles';
 
 const Header = () => (
   <HeaderStyles>
-    <HeaderImg
-      src="/static/public/images/crunchaccounting.png"
-      alt="Crunch Accounting Logo"
-    ></HeaderImg>
+    <HeaderImg src={CrunchAccounting} alt="Crunch Accounting Logo"></HeaderImg>
   </HeaderStyles>
 );
 
