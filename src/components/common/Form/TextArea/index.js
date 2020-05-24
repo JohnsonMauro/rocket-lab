6d6e3dae39d754a426ae1a5ff61bfc6f9d97c367
Johnson Mauro <johnsonmauro@gmail.com>
2020-05-24 03:09:16 -0300

    improvement TextArea component :zap:

--- src/components/common/Form/TextArea/index.js
+++ src/components/common/Form/TextArea/index.js
@@ -1,7 +1,20 @@
+import ErrorMessage from '../Message/ErrorMessage';
 import { TextAreaStyles } from './styles';
 
-const TextArea = ({ id, name, rows }) => (
-  <TextAreaStyles id={id} name={name} rows={rows}></TextAreaStyles>
+const TextArea = ({ error, id, maxLength, minLength, name, onChange, required, rows }) => (
+  <>
+    <TextAreaStyles
+      error={error}
+      id={id}
+      maxLength={maxLength}
+      minLength={minLength}
+      name={name}
+      onChange={onChange}
+      required={required}
+      rows={rows}
+    ></TextAreaStyles>
+    {!!error && <ErrorMessage>{error}</ErrorMessage>}
+  </>
 );
 
 export default TextArea;
--- src/components/common/Form/TextArea/styles.js
+++ src/components/common/Form/TextArea/styles.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 const TextAreaStyles = styled.textarea`
-  border: 0.2px solid ${({ theme }) => theme.colors.gainsboro};
+  border: 0.2px solid ${({ error, theme }) => (error ? theme.colors.error : theme.colors.gainsboro)};
   border-radius: 1.8px;
   display: block;
   width: 100%;
